0d9cd1661c0fcdf61830a190513e4bed904a604c
Sheeraz-Ahmed-11 <isheeraz11@gmail.com>
2026-03-31 00:52:56 +0500

    Update app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,20 +1,10 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-const cors = require("cors");
 const routes = require("./routes/todo.routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: [
-    'https://to-do-web-app-ochre.vercel.app',
-    'https://to-do-web-app-git-main-sheeraz-ahmed-11s-projects.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
 app.use(cookieParser());
 
 
