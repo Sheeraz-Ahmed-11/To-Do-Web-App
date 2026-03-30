179499e00f76aea05501425f7191e0e0b84f24ca
Sheeraz-Ahmed-11 <isheeraz11@gmail.com>
2026-03-31 01:06:28 +0500

    Update app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,7 +1,16 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
-const cors = require('cors');
 const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const routes = require("./routes/todo.routes");
 
 app.use(cors({
@@ -10,13 +19,12 @@ app.use(cors({
     if (!origin || allowed.some(p => p.test(origin))) callback(null, true);
     else callback(new Error('Not allowed by CORS'));
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT, DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
-app.options('*', cors());  // ← ADD THIS LINE
-
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
