46eedbd99120b08611994036609f6eb1a042d117
Sheeraz-Ahmed-11 <isheeraz11@gmail.com>
2026-03-31 00:59:44 +0500

    Remove CORS configuration and fix console log

--- Server/server.js
+++ Server/server.js
@@ -1,22 +1,10 @@
 require('dotenv').config();
 const server = require('./src/app');
-const cors = require('cors);
 const connectToDb = require('./src/config/db');
-const port = process.env.PORT;
 
+const port = process.env.PORT;
 connectToDb();
 
-app.use(cors({
-  origin: (origin, callback) => {
-    const allowed = [/^https:\/\/to-do-web-app.*\.vercel\.app$/];
-    if (!origin || allowed.some(p => p.test(origin))) callback(null, true);
-    else callback(new Error('Not allowed by CORS'));
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
 server.listen(port, () => {
-    console.log("server is runing on port:", port)
+    console.log("server is running on port:", port);
 });
