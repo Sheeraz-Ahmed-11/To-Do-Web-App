c4d1d144ecf9095fe0f3abf9cc87457156255074
Sheeraz-Ahmed-11 <isheeraz11@gmail.com>
2026-03-31 00:59:45 +0500

    Add CORS middleware for handling cross-origin requests

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,13 +1,23 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const routes = require("./routes/todo.routes");
+
+app.use(cors({
+  origin: (origin, callback) => {
+    const allowed = [/^https:\/\/to-do-web-app.*\.vercel\.app$/];
+    if (!origin || allowed.some(p => p.test(origin))) callback(null, true);
+    else callback(new Error('Not allowed by CORS'));
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-
 app.use('/', routes);
 
 module.exports = app;
